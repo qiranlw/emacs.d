ca93efd0cee63f57a63db8efb683c5e7592e4ee4
Qi Ran <qiranlw@gmail.com>
2019-01-01 21:06:14 +0800

    update init.el

--- init.el
+++ init.el
@@ -3,7 +3,65 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;; (package-initialize)
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  )
+
+(require 'cl)
+
+(defvar qiranlw/packages '(
+			   company
+			   monokai-theme
+			   hungry-delete
+			   swiper
+			   counsel
+			   smartparens
+			   js2-mode
+			   nodejs-repl
+			   ) "Default packages")
+
+(setq package-selected-packages qiranlw/packages)
+
+(defun qiranlw/packages-installed-p ()
+  (loop for pkg in qiranlw/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+(unless (qiranlw/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg qiranlw/packages)
+    (when (not (package-installed-p pkg))
+	  (package-install pkg))))
+
+;; 饥饿删除设置，可以一次删除多个空格
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+;; swiper 配置
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+
+;; smartparens 配置
+(require 'smartparens-config)
+;; (add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(smartparens-global-mode t)
+
+;; config js2-mode for js files
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
 
 ;; 关闭工具栏
 (tool-bar-mode -1)
@@ -58,15 +116,19 @@
 ;; 光标所在行高亮显示
 (global-hl-line-mode t)
 
+;; 启动 monokai 主题
+(load-theme 'monokai t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company))))
+ '(company-idle-delay 0.1)
+ '(company-minimum-prefix-length 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(js2-external-variable ((t (:foreground "dark gray")))))
